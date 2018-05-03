8a222e807025d126a65f9024c56a0ac8b9f1e85a
c0reac0de <candice.correa@gmail.com>
2018-05-02 20:05:15 -0400

    third

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,41 @@
 //Responsive Java-->
 //<!-- display computer random value in box-->
+//create empty div/variable
+//assign random value
+//connect and display random value to compGuess div
+$(document).ready(function(){
+    $(".jumbotron").on("click","#compGuess",function(){
+        var randomNum="";
+        var random=Math.floor(Math.random()*100)+1;
+        randomNum=random+randomNum;
+        $("#compGuess").innerHTML(randomNum);
+    });
+
+
 //<!-- connect crystal img with on clicks-->
-//<!-- give crystal random values (resets with refresh)-->
+//create crystal variable
+//assign variable a random value between 1-9
+//connect crytal variable to crystal images
+//reset variable values with win/lose
+
+
+$(".jumbotron").keyup("click","#crystals", function(){
+    var crystalValue="";
+    var randomCtlVal=Math.floor(Math.random)(0>9)+1;
+    crystalValue=randomCtlVal+crystalValue;
+    console.log(crystalValue);
+});
+
+
+
+
+
+
+
+
+
 //<!-- connect total score with crystal clicks AND function adder-->
 //<!-- connect crystal value box with crystal clicks-->
 
 
+});
